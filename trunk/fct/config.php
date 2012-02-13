1910ad456739f011796586a88d738774ba47df74
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-02-13 09:50:59 +0000

    - Suppression de la variable globale $NiveauMaxBatiment pour l'incorporer en tant que variable constant que la class "batiment" - Ajout de la nouvelle constante batiment::NIVEAU_MAX - Application de cette constant dans les différents script

--- trunk/fct/config.php
+++ trunk/fct/config.php
@@ -1,6 +1,5 @@
 <?php 
-global 	$NumVersion, $nbLigneCarte, $nbColonneCarte, $db, $temp_combat, $NiveauMaxBatiment, 
-		$VieMaximum, $arCouleurs, $lstBatimentConstructionUnique, $MAX_essai, $lstPoints,
+global 	$NumVersion, $nbLigneCarte, $nbColonneCarte, $db, $temp_combat, $VieMaximum, $arCouleurs, $lstBatimentConstructionUnique, $MAX_essai, $lstPoints,
 		$objManager, $CodeCouleurQuete, $lstNonBatiment, $chkDebug;
 
 $DB_serveur			= 'localhost'; 			// Nom du serveur
@@ -15,7 +14,6 @@ $nbColonneCarte		= 13;					// Nombre de colonne de la carte
 $nbCarteH			= 5;					// Nombre de carte horizontale
 $nbCarteV			= 5;					// Nombre de carte Verticale
 $temp_combat		= 3600 * 1;				// Temp entre chaque combat
-$NiveauMaxBatiment	= 5;					// Niveau Maximum pour chaque batiment.
 $VieMaximum			= 300;					// Limite de vie maximum
 
 $chkDebug			= false;
