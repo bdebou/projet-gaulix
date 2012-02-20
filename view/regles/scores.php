a5985a8b493644047b86a852ab2ad8f64b17add1
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-02-20 13:19:33 +0000

    Extrraction de l'affichage du tableau des points pour l'insérer sur la page "Scores.php"

--- view/regles/scores.php
+++ view/regles/scores.php
@@ -1,3 +1,12 @@
-<?php
-echo utf8_decode(file_get_contents('http://code.google.com/p/projet-gaulix/wiki/scores?show=content'));
-?>
+<h1>Scores</h1>
+<p>Vous trouverez la liste des joueurs avec un apercçu de leur status, le nombre de combats gagnés et perdus.</p>
+<p>Pour info, les combats dont les résultats sont nuls ne sont pas comptabilisés.</p>
+<div style="clear:both;float:left;margin-right:15px;">
+	<?php echo AfficheTableauGainScores();?>
+</div>
+<div style="float:left; width:400px;">
+	<p>Pour certaines actions, un nombre X de points vous sera attribué et un tableau des scores sera créé.</p>
+	<p>Pour la liste des points attribué pour chaque action, voir tableau ci-contre.</p>
+	<p>Chaque <u>construction de batiment</u> vous rapportera un nombre spécifique de points, mais vous les predrez si il est détruit.</p>
+	<p>Chaque <u>Quête</u> vous apporte un nombre spécifique de points.</p>
+</div>
