a5f6f7cd3430c0b606bd6c258f87d3b635345db6
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-02-20 13:19:33 +0000

    Extrraction de l'affichage du tableau des points pour l'insérer sur la page "Scores.php"

--- trunk/view/regles/general.php
+++ trunk/view/regles/general.php
@@ -8,6 +8,7 @@
 <div style="float:left; width:500px;">
 	<p>Pour passer au niveau suivant, vous devez atteindre un niveau X d'expérience. Voir tableau ci-contre.</p>
 	<p>Plus votre niveau est élevé, plus vous trouverez des objets intéressants, vous aurez des quêtes intéressantes.</p>
+	<p>Pour chaque action, des points vous seront attribués. Pour en connaitre le détail, allez voir dans <a href="#" onclick="javascript:change_onglet('scores');">Scores</a>.</p>
 </div>
 <div style="float:left;">
 	<table class="experience">
@@ -19,30 +20,22 @@
 		<tr><td>...</td><td>...</td></tr>
 	</table>
 </div>
-
-<div style="clear:both;float:left;margin-right:15px;">
-	<?php echo AfficheTableauGainScores();?>
-</div>
-<div style="float:left; width:400px;">
-	<p>Pour certaines actions, un nombre X de points vous sera attribué et un tableau des scores sera créé.</p>
-	<p>Pour la liste des points attribué pour chaque action, voir tableau ci-contre.</p>
-	<p>Chaque <u>construction de batiment</u> vous rapportera un nombre spécifique de points, mais vous les predrez si il est détruit.</p>
-	<p>Chaque <u>Quête</u> vous apporte un nombre spécifique de points.</p>
-</div>
-	<h2 style="clear:both;">Combats</h2>
-	<p>Vous pouvez combattre d'autres gaulois ou attaquer des bâtiments ou bien encore des monstres de quête.</p>
-	<p>Entre chaque combat, il devra se passer au minimum <?php echo ($temp_combat / 3600);?>hrs. Ceci uniquement pour ne pas s'acharner sur la victime et lui laisser peut-être une chance.</p>
-		<h3>Combats entre joueur</h3>
-		<p>Le résultat du combat sera calculé selon la formule suivant :</br>
-			Valeur de l'attaquant : (<var>Attaque</var><strong> * 1.15</strong>) + <var>Défense</var></br>
-			Valeur de la cible : <var>Attaque</var> + <var>Défense</var></br>
-			</br>
-			
-		Cette valeur est calculée pour votre adversaire également et une différence en est tirée. 
-		Celui qui a la valeur de combat la plus élevée gagne le combat. Le gagnant du combat augmente son expérience de 5pts et vole un peu d'or au perdant. 
-		Le perdant, lui, perd de l'or et des pts de vie correspondant à la différence des 2 valeurs de combats.</p>
-		<h3>Attaques de bâtiment</h3>
-		<p>C'est le même principe que pour le combat entre joueur mais la différence se situe au niveau des conséquences.</p>
-		<p>Le bâtiment perdra des pts de vie selon la formule suivante : <i>ValeurCombatJoueur - DefenseBatiment</i>. Si la différence est négative, le bâtiment ne perdra pas de points.</p>
-		<p>Et le joueur perdra des pts de vies selon la formule suivante : <i>(AttaqueBatiment * 1.15) - DefenseJoueur</i>. Si la différence est négative, le joueur ne perdra pas de points.</p>
-		<p>Dans tous les cas, le joueur augmentera son expérience de 5pts.</p>
+<h2 style="clear:both;">Combats</h2>
+<p>Vous pouvez combattre d'autres gaulois ou attaquer des bâtiments ou bien encore des monstres de quête.</p>
+<p>Entre chaque combat, il devra se passer au minimum <?php echo ($temp_combat / 3600);?>hrs. Ceci uniquement pour ne pas s'acharner sur la victime et lui laisser peut-être une chance.</p>
+	<h3>Combats entre joueur</h3>
+	<p>Pour le résultat du combat, 2 valeurs seront calculées selon les formules suivantes :</p>
+		<ul>
+			<li>Valeur de l'attaquant : (<var>Attaque</var><strong> * <?php echo personnage::TAUX_ATTAQUANT;?></strong>) + <var>Défense</var></li>
+			<li>Valeur de la cible : <var>Attaque</var> + <var>Défense</var></li>
+		</ul>
+		<p>Le résultat du combat dépendra de la comparaison de ces 2 valeurs. Celui qui a la valeur de combat la plus élevée gagne le combat.</p>
+		<ul>
+			<li>Le gagnant du combat augmente son expérience de 5pts et vole un peu d'or au perdant.</li> 
+			<li>Le perdant, lui, perd de l'or et des pts de vie correspondant à la différence des 2 valeurs de combats.</li>
+		</ul>
+	<h3>Attaques de bâtiment</h3>
+	<p>C'est le même principe que pour le combat entre joueur mais la différence se situe au niveau des conséquences.</p>
+	<p>Le bâtiment perdra des pts de vie selon la formule suivante : <i>ValeurCombatJoueur - DefenseBatiment</i>. Si la différence est négative, le bâtiment ne perdra pas de points.</p>
+	<p>Et le joueur perdra des pts de vies selon la formule suivante : <i>(AttaqueBatiment * 1.15) - DefenseJoueur</i>. Si la différence est négative, le joueur ne perdra pas de points.</p>
+	<p>Dans tous les cas, le joueur augmentera son expérience de 5pts.</p>
