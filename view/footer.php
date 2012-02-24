fd37e9b7b30903dd28a6ab6c07b1a33ccb3580c2
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-02-24 13:19:42 +0000

    Ajout du boutton PayPal pour un don éventuel

--- view/footer.php
+++ view/footer.php
@@ -13,6 +13,9 @@
 							</a>
 						</p>
 					</td>
+					<td style="text-align:center;">
+						<?php include('view/button/paypal_don.html');?>
+					</td>
 					<td style="text-align:right;">
 						<a href="http://validator.w3.org/check?uri=referer" style="text-decoration:none;" target="_blank">
 							<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
