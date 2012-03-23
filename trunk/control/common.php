5249cd6dddf1267c2f338f1699ac8e5c32d690f0
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-03-23 14:18:01 +0000

    - Correction syntaxe HTML5 pour appel de JavaScript - Remplacement de la variable $_SESSION['main']['Loginstatus'] par $_SESSION['Loginstatus'] - Adapation de certain module pour utiliser cette modification - Correction Orthographe "Marcher" en "Marche"

--- trunk/control/common.php
+++ trunk/control/common.php
@@ -2,9 +2,13 @@
 global $objManager, $chkDebug;
 
 $oJoueur = $objManager->GetPersoLogin($_SESSION['joueur']);
-
+print_r($_SESSION);
 if($chkDebug){
-	echo '$_SESSION[\'main\']<br />';print_r($_SESSION['main']['LoginStatus']);echo '<br />';
+	if(isset($_SESSION['LoginStatus'])){
+		echo '$_SESSION[\'LoginStatus\']<br />';print_r($_SESSION['LoginStatus']);echo '<br />';
+	}else{
+		echo '$_SESSION[\'LoginStatus\']) not set<br />';
+	}
 	echo '$_GET<br />';print_r($_GET);echo '<br />';
 	echo '$_POST<br />';print_r($_POST);echo '<br />';
 	echo '<hr />';
@@ -23,16 +27,20 @@ if(isset($_GET['action'])){
 }
 
 if($chkDebug){
-	print_r($_SESSION['main']['LoginStatus']);
+	if(isset($_SESSION['LoginStatus'])){
+		print_r($_SESSION['LoginStatus']);
+	}else{
+		echo '$_SESSION[\'LoginStatus\']) not set<br />';
+	}
 }
 
 $objManager->update($oJoueur);
 unset($oJoueur);
 
 if($chkDebug OR !$chkErr){
-	echo '<br /><a href="index.php?page='.$_GET['retour'].'">Retour</a>';
+	echo '<br /><a href="index.php?page='.(isset($_GET['retour'])?$_GET['retour']:'main').'">Retour</a>';
 }elseif($CheckRetour){
 	//header('location: index.php?page='.$_GET['retour']);
-	echo '<script language="javascript">window.location="index.php?page='.$_GET['retour'].'";</script>';
+	echo '<script type="text/javascript">window.location="index.php?page='.(isset($_GET['retour'])?$_GET['retour']:'main').'";</script>';
 }
 ?>
