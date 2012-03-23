95f2d6744abb3a8361db658abd468788a3e98b63
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-03-23 14:22:57 +0000

    - Modification apparence de la carte générale pour ne plus afficher d'espace entre petite carte ni de cadrillage. - Divers petites modifs

--- view/cartes.php
+++ view/cartes.php
@@ -1,6 +1,6 @@
 <div class="main">
 	<h1>La Carte</h1>
-	<table style="border:none;" class="all_cartes">
+	<table class="all_cartes">
 <?php 
 $arCarteNum = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y');
 $numCol = 0;
