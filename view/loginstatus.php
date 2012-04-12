44548437455f72ee2a761a428f498bd6b042c5e3
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-04-12 13:23:10 +0000

    - Remplacement de l'appel de fonction FoundBatiment(1) par personnage::GetObjSaMaison() - Affichage de son grade au lieu de son niveau

--- view/loginstatus.php
+++ view/loginstatus.php
@@ -11,9 +11,11 @@ $arDef = $oJoueur->GetDefPerso();
 	<a id="TopPage"></a>
 	<table class="loginstatus">
 		<tr>
-			<td style="background-color: Brown; font-weight:bold; text-transform:uppercase; width:200px;">
-				<?php echo $oJoueur->GetLogin();?> (<?php echo $oJoueur->GetNiveau();?>)
-				<?php echo AfficheRecompenses($oJoueur->GetLogin(), $oJoueur->GetClan());?>
+			<td style="background-color: Brown; width:200px;">
+				<span class="login"><?php echo $oJoueur->GetLogin();?></span><?php echo AfficheRecompenses($oJoueur->GetLogin(), $oJoueur->GetClan());?>
+				<br />
+				<?php echo ucfirst(GetInfoCarriere($oJoueur->GetCodeCarriere(),'carriere_nom'));?>
+				
 			</td>
 			<td style="width:150px;">
 				<img alt="Barre de Vie" src="./fct/fct_image.php?type=vie&amp;value=<?php echo $oJoueur->GetVie();?>&amp;max=<?php echo personnage::VIE_MAX;?>" />
@@ -24,7 +26,7 @@ $arDef = $oJoueur->GetDefPerso();
 			<td style="background-color:<?php echo $arCouleurs['Or'];?>; width:145px;">
 				<?php echo AfficheIcone('or');?> : <?php echo $oJoueur->GetArgent();?>
 			</td>
-			<?php if(!is_null(FoundBatiment(1))){?>
+			<?php if(!is_null($oJoueur->GetObjSaMaison())){?>
 			<td style="background-color:<?php echo $arCouleurs['Nourriture'];?>;">
 				<?php echo AfficheRessource('Nourriture', $oJoueur)?>
 			</td>
@@ -44,7 +46,7 @@ $arDef = $oJoueur->GetDefPerso();
 			<td style="background-color:<?php echo $arCouleurs['Defense'];?>;">
 				<?php echo AfficheIcone('defense');?> : <?php echo $arDef['0'];?> (<?php echo $arDef['1'];?>)
 			</td>
-			<?php if(!is_null(FoundBatiment(1))){?>
+			<?php if(!is_null($oJoueur->GetObjSaMaison())){?>
 			<td style="background-color:<?php echo $arCouleurs['Pierre'];?>;">
 				<?php echo AfficheRessource('Pierre', $oJoueur)?>
 			</td>
