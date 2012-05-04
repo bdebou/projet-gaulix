1a138a3a7d693a5410f41d498973cdaa1fbbee68
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-05-04 10:00:42 +0000

    Ajout de la constante tour::POINT_TOUR_ATTAQUE

--- fct/tour.class.php
+++ fct/tour.class.php
@@ -1,5 +1,9 @@
 <?php
 class tour extends batiment{
+	
+	//Les points
+	const POINT_TOUR_ATTAQUE		= 2;
+	
 	//--- fonction qui est lancer lors de la création de l'objet. ---
 	public function __construct(array $carte, array $batiment){
 		$this->Hydrate($carte, $batiment);
