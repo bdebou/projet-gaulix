3973b98d7ddc5d43468e50e220a84e890953ed55
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-05-05 13:42:20 +0000

    - Ajout de 2 variables globale $lstBatimentsNonConstructible et $lstTypeObjets - Utilisation des constantes de points des différentes class. - Adaptation des listes de batiments constructible unique ou non et les non-constructibles

--- trunk/fct/config.php
+++ trunk/fct/config.php
@@ -1,6 +1,6 @@
 <?php 
 global 	$NumVersion, $nbLigneCarte, $nbColonneCarte, $db, $temp_combat, $arCouleurs, $lstBatimentConstructionUnique, $MAX_essai, $lstPoints,
-		$objManager, $CodeCouleurQuete, $lstNonBatiment, $chkDebug;
+		$objManager, $CodeCouleurQuete, $lstNonBatiment, $chkDebug, $lstBatimentsNonConstructible, $lstTypeObjets;
 
 date_default_timezone_set('Europe/Brussels');
 
@@ -31,15 +31,20 @@ $arCouleurs			= array('Attaque'		=> '#FF0000',					// Red
 							'Or'			=> '#FFD700');					// Gold
 
 //Listes des batiments que l'on ne peut construire que 1 seule fois
-$lstBatimentConstructionUnique	= array(4,		//Entrepot
-										5,		//Banque
+$lstBatimentConstructionUnique	= array(1,		//Maison
+										4,		//Entrepot
+										5,		//Potager
 										6,		//Ferme
 										9,		//Marche
-										10);	//Mine
+										10,		//Mine
+										16);	//Carrière de pierre
 
-//Liste des batiments disponible
-$lstNonBatiment		= array(11 /*Mer du Nord*/);	
+//Liste des batiments Non constructible
+$lstBatimentsNonConstructible	= array(7/*Res Pierre*/, 8/*Res Bois*/);
+$lstNonBatiment					= array(11 /*Mer*/, 12 /*Côtes*/, 13 /*Rivière*/, 14/*Montagne*/, 15/*Dolmen*/);	
 
+//Liste des type d'objets
+$lstTypeObjets					= array('Ressource', 'Divers', 'Armement', 'Construction');
 //Liste des couleurs pour les quetes
 $CodeCouleurQuete	= array('monstre'	=> '#ff6464',
 							'recherche'	=> '#82ff82',
@@ -48,17 +53,17 @@ $CodeCouleurQuete	= array('monstre'	=> '#ff6464',
 							'romains'	=> '#ff6464');
 
 //Liste des Points
-$lstPoints	= array('CombatGagné'	=>	array(10,	'Combat gagné'),
-					'CombatPerdu'	=> array(-10,	'Combat perdu'),
-					'AttBatAdvers'	=> array(3,		'Attaque de batiment adverse'),
-					'BatAbimé'		=> array(-3,	'Un de vos batiment a été attaqué'),
-					'AttTour'		=> array(-2,	'Attaque d\'une tour'),
-					'CmpTerminé'	=> array(5,		'1 Niveau de compétence terminé'),
-					'NivTerminé'	=> array(15,	'Passé 1 niveau'),
-					'BatRéparé'		=> array(8,		'Batiment réparé à 100%'),
-					'ObjFabriqué'	=> array(1,		'Objet fabriqué'),
-					'BatDetruit'	=> array(20,	'Vous avez détruit un batiment adverse'),
-					'PersoTué'		=> array(-150,	'Personnage tué'));
+$lstPoints	= array('CombatGagné'	=>	array(abs(personnage::POINT_COMBAT),				'Combat gagné'),
+					'CombatPerdu'	=> array((abs(personnage::POINT_COMBAT) * -1),			'Combat perdu'),
+					'AttBatAdvers'	=> array(abs(batiment::POINT_BATIMENT_ATTAQUE),			'Attaque de batiment adverse'),
+					'BatAbimé'		=> array((abs(batiment::POINT_BATIMENT_ATTAQUE) * -1),	'Un de vos batiment a été attaqué'),
+					'AttTour'		=> array((abs(tour::POINT_TOUR_ATTAQUE) * -1),			'Attaque d\'une tour'),
+					'CmpTerminé'	=> array(abs(personnage::POINT_COMPETENCE_TERMINE),		'1 Niveau de compétence terminé'),
+					'NivTerminé'	=> array(abs(personnage::POINT_NIVEAU_TERMINE),			'Passé 1 niveau'),
+					'BatRéparé'		=> array(abs(batiment::POINT_BATIMENT_REPARE),			'Batiment réparé à 100%'),
+					'ObjFabriqué'	=> array(abs(personnage::POINT_OBJET_FABRIQUE),			'Objet fabriqué'),
+					'BatDetruit'	=> array(abs(batiment::POINT_BATIMENT_DETRUIT),			'Vous avez détruit un batiment adverse'),
+					'PersoTué'		=> array((abs(personnage::POINT_PERSO_TUE) * -1),		'Personnage tué'));
 
 
 // On se connecte au serveur MySQL
