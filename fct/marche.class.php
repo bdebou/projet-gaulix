e051fa470c62f8dfcb02cf35bc72cce0c5136225
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-05-25 13:06:51 +0000

    Ajout de la constante ID_BATIMENT

--- fct/marche.class.php
+++ fct/marche.class.php
@@ -1,14 +1,15 @@
 <?php
 class marche extends batiment{
+	
+	const ID_BATIMENT		= 9;
+	
 	//--- fonction qui est lancer lors de la création de l'objet. ---
 	public function __construct(array $carte, array $batiment){
-		$this->Hydrate($carte, $batiment);
-	}
-	public function Hydrate(array $carte, array $batiment){
 		date_default_timezone_set('Europe/Brussels');
 		
 		parent::Hydrate($carte, $batiment);
 	}
+	
 	
 	//Les Affichages
 	//--------------
