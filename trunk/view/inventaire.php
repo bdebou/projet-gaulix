0af1d71c3246af8f8aa25546b5f1fe75682a763c
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-05-31 12:44:17 +0000

    Petite modification de l'affichage des info des armements.

--- trunk/view/inventaire.php
+++ trunk/view/inventaire.php
@@ -125,9 +125,9 @@ foreach($lstTypeObjets as $Category)
 					break;
 				case 'Armement':
 					$txtInfoArmement = '<tr>'
-											.'<td>'.$objObjet->GetAttaque().' '.AfficheIcone(objArmement::TYPE_ATTAQUE).'</td>'
-											.'<td>'.$objObjet->GetDefense().' '.AfficheIcone(objArmement::TYPE_DEFENSE).'</td>'
-											.'<td>'.$objObjet->GetDistance().' '.AfficheIcone(objArmement::TYPE_DISTANCE).'</td>'
+											.'<td>'.AfficheIcone(objArmement::TYPE_ATTAQUE).' : '.$objObjet->GetAttaque().'</td>'
+											.'<td>'.AfficheIcone(objArmement::TYPE_DEFENSE).' : '.$objObjet->GetDefense().'</td>'
+											.'<td>'.AfficheIcone(objArmement::TYPE_DISTANCE).' : '.$objObjet->GetDistance().'</td>'
 										.'</tr>';
 					$nbColonne = 3;
 					$nbLigne = 5;
@@ -142,15 +142,15 @@ foreach($lstTypeObjets as $Category)
 					<img src="./img/objets/'.$objObjet->GetCode().'.png" alt="'.$objObjet->GetNom().'" width="100px" onmouseover="montre(\''.CorrectDataInfoBulle($objObjet->GetInfoBulle()).'\');" onmouseout="cache();" style="vertical-align:middle;" />
 					</td></tr>'
 					.'<tr><th colspan="'.$nbColonne.'">'.($objObjet->GetQuantite() > 1?'<b>'.$objObjet->GetQuantite().'x</b> ':'').$objObjet->GetNom().$txtAbandonner.'</th></tr>'
+					.$txtInfoArmement
 					.'<tr><td colspan="'.$nbColonne.'" style="text-align:center;">
 						<input style="width:150px;" id="Slider'.$objObjet->GetCode().'" type="range" min="1" max="'.$objObjet->GetQuantite().'" step="1" value="1" onchange="printValue(\'Slider'.$objObjet->GetCode().'\',\'RangeValue'.$objObjet->GetCode().'\');" />
 						<input style="width:50px;" name="qte" id="RangeValue'.$objObjet->GetCode().'" onchange="printValue(\'RangeValue'.$objObjet->GetCode().'\',\'Slider'.$objObjet->GetCode().'\');" type="number" min="1" max="'.$objObjet->GetQuantite().'" step="1" value ="1" size="15" />
 						<script>printValue(\'Slider'.$objObjet->GetCode().'\',\'RangeValue'.$objObjet->GetCode().'\');</script>
 					</td></tr>'
-					.'<tr><td class="action_inventaire">'.$txtVendre.'</td><td style="text-align:center;">'.AfficheListePrix(array(personnage::TYPE_RES_MONNAIE.'='.$objObjet->GetPrix())).' par unité</td></tr>'
+					.'<tr><td class="action_inventaire">'.$txtVendre.'</td><td colspan="'.($nbColonne - 1).'" style="text-align:center;">'.AfficheListePrix(array(personnage::TYPE_RES_MONNAIE.'='.$objObjet->GetPrix())).' par unité</td></tr>'
 					.(!is_null($txtConvertion)?'<tr><td class="action_inventaire">'.$txtConvertir.'</td><td style="text-align:center;">'.$txtConvertion.' par unité</td>':'')
 					.'<input type="hidden" name="id" value="'.$id.'" />'
-					.$txtInfoArmement
 					.((!is_null($txtEntreposer) OR !is_null($txtUtiliser) OR !is_null($txtEquiper))?'<tr><td colspan="'.$nbColonne.'" class="action_inventaire">'.$txtEntreposer.$txtUtiliser.$txtEquiper.'</td></tr>':NULL)
 	.'</table>'
 	.'</form>';
