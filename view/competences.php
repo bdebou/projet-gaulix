bf4a2f484c58d40b29b5507ab384fc2cf560fa62
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-06-08 13:14:11 +0000

    - Adaptation de la gestion de l'inscription aux compétences - Utilisation d'un formulaire POST au lieu d'un simple bouton

--- view/competences.php
+++ view/competences.php
@@ -1,18 +1,30 @@
 <div class="main">
 <h1>Compétences</h1>
+<?php
+	global $objManager;
+	
+	$oJoueur = $objManager->GetPersoLogin($_SESSION['joueur']);
+	
+	$oMaison = $oJoueur->GetObjSaMaison();
+?>
 <table class="perfectionnement">
 	<tr>
 		<td style="width:50%;">
-			<?php echo AfficheModulePerfectionnement('attaque');?>
+			<?php echo AfficheModulePerfectionnement(objArmement::TYPE_ATTAQUE, $oJoueur, $oMaison);?>
 		</td>
 		<td>
-			<?php echo AfficheModulePerfectionnement('defense');?>
+			<?php echo AfficheModulePerfectionnement(objArmement::TYPE_DEFENSE, $oJoueur, $oMaison);?>
 		</td>
 	</tr>
 	<?php
-	$temp = AfficheAutreCompetences();
+	$temp = AfficheAutreCompetences($oJoueur, $oMaison);
 	echo $temp[0];
 	?>
 	</table>
 	<?php echo $temp[1];?>
 </div>
+<?php 
+	$objManager->UpdateBatiment($oMaison);
+	$objManager->update($oJoueur);
+	unset($oJoueur, $oMaison);
+?>
