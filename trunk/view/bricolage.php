7aa0cc87cc0d74773087a465cd22cf6d29c91be5
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-06-08 13:27:02 +0000

    Ajout du JavaScript code pour updater les valeurs du slider et du number dés que ca change

--- trunk/view/bricolage.php
+++ trunk/view/bricolage.php
@@ -1,5 +1,25 @@
 <div class="main">
+<script>
+function printValue(ValueFrom, ValueTo) {
+	var x = document.getElementById(ValueTo);
+	var y = document.getElementById(ValueFrom);
+	x.value = y.value;
+}
+
+</script>
+
+<?php
+	global $objManager;
+	
+	$oJoueur = $objManager->GetPersoLogin($_SESSION['joueur']);
+?>
+
 <h1>Bricolages</h1>
 <p>Vous devez avoir tous les éléments dans votre Bolga pour pouvoir bricoler quelque chose. Donc allez vite à votre entrepôt récupérer les éléments manquant.</p>
-<?php echo AfficheListeElementBricolage((isset($_GET['onglet'])?$_GET['onglet']:null));?>
+<?php echo AfficheListeElementBricolage($oJoueur, (isset($_GET['onglet'])?$_GET['onglet']:null));?>
 </div>
+
+<?php 
+	$objManager->update($oJoueur);
+	unset($oJoueur);
+?>
