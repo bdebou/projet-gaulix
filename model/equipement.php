0f3bc52c4f86b214faa86df11a34924a2948100d
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-07-24 08:34:14 +0000

    Move du module AfficheEquipement() de model/common.php vers model/equipement.php

--- model/equipement.php
+++ model/equipement.php
@@ -1,4 +1,81 @@
 <?php
+function AfficheEquipement($type, personnage &$oJoueur) {
+	$SizeHeight = 50;
+
+	switch ($type) {
+		case '1': $CodeObjet = $oJoueur->GetCasque();	break;
+		case '2': $CodeObjet = $oJoueur->GetBouclier();	break;
+		case '3': $CodeObjet = $oJoueur->GetJambiere();	break;
+		case '4':
+			$CodeObjet = $oJoueur->GetCuirasse();
+			$SizeHeight = 150;
+			break;
+		case '5':
+			$CodeObjet = $oJoueur->GetArme();
+			$SizeHeight = 150;
+			break;
+		case '6': $CodeObjet = $oJoueur->GetSac();		break;
+		case '7': $CodeObjet = $oJoueur->GetLivre();	break;
+	}
+
+	if (is_null($CodeObjet)) {
+		return '&nbsp;';
+	}
+
+	//if ($_SESSION['main']['uri'] == 3
+	if ($_GET['page'] == 'inventaire'
+	AND count($oJoueur->GetLstInventaire()) < $oJoueur->QuelCapaciteMonBolga()
+	AND !in_array($type, array(7))) {
+		$chkLink = true;
+	} else {
+		$chkLink = false;
+	}
+
+	if (in_array($type, array(7))) {
+		if ($CodeObjet == 'NoBook') {
+			if (is_null($oJoueur->GetLstSorts())) {
+				return '&nbsp;';
+			} else {
+				$arSort = explode('=', current($oJoueur->GetLstSorts()));
+				$CodeObjet = $arSort[0];
+			}
+		} else {
+			//on affiche le livre
+			if(!is_null($oJoueur->GetLstSorts())){
+				$InfoBulle = '<table class="equipement">';
+				foreach ($oJoueur->GetLstSorts() as $Sort) {
+					$arSort = explode('=', $Sort);
+					$sql = "SELECT * FROM table_objets WHERE objet_code='" . strval($arSort[0]) . "';";
+					$requete = mysql_query($sql) or die(mysql_error() . '<br />' . $sql);
+					$result = mysql_fetch_array($requete, MYSQL_ASSOC);
+
+					$InfoBulle .= '<tr><th colspan="2">' . $result['objet_nom'] . '</th></tr>'
+					. '<tr>'
+					. '<td><img src="./img/objets/' . $arSort['0'] . '.png" alt="' . $result['objet_nom'] . '" height="50px" /></td>'
+					. '<td>' . $result['objet_description'] . '</td>'
+					. '</tr>';
+				}
+				$InfoBulle .= '</table>';
+			}else{$InfoBulle = '<table class="equipement"><tr><th>Votre livre est vide.</th></tr></table>';
+			}
+		}
+
+		return ($chkLink ?'
+			<a href="./fct/main.php?action=unuse&amp;id=' . $type . '">' : '')
+		. '<img src="./img/objets/' . $CodeObjet . '.png" '
+		. 'height="' . $SizeHeight . '" '
+		. 'alt="Livre de sort" '
+		. 'onmouseover="montre(\'' . CorrectDataInfoBulle($InfoBulle) . '\');" '
+		. 'onmouseout="cache();" '
+		. '/>'
+		. ($chkLink ?'</a>' : '');
+	} else {
+		return ($chkLink ?'
+    		<a href="./fct/main.php?action=unuse&amp;id=' . $type . '">' : '')
+		.AfficheInfoObjet($CodeObjet, $SizeHeight)
+		. ($chkLink ?'</a>' : '');
+	}
+}
 function AfficheDescriptifEquipement(personnage &$oJoueur){
 	$txt = '
 	<table class="equipement">
