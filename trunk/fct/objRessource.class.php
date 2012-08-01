bb5070c32d570e76f784aaab418d971e789b1702
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-08-01 15:12:26 +0000

    Exptraction du module Hydrate pour etre apellé par ses sous-class

--- trunk/fct/objRessource.class.php
+++ trunk/fct/objRessource.class.php
@@ -4,17 +4,20 @@ class objRessource extends objMain{
 	public function __construct(array $data, $nb){
 		date_default_timezone_set('Europe/Brussels');
 	
+		$this->Hydrate($data, $nb);
+	}
+	
+	public function Hydrate($data, $nb){
 		parent::Hydrate($data, $nb);
 		
 		foreach($data as $key=>$value)
 		{
 			switch($key)
 			{
-				
+		
 			}
 		}
 	}
-	
 	//Les GETS
 	//========
 	public function GetNb($Type){
