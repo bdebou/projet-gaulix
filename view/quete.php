97106169d17f95ca7b42a715b81ebb5bc8a2b2ad
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-09-28 13:54:46 +0000

    Ajout du check si on est sur la maison pour activer l'inscription ou la validation des Quetes

--- view/quete.php
+++ view/quete.php
@@ -1,33 +1,42 @@
 <?php 
 global $objManager;
 $oJoueur = $objManager->GetPersoLogin($_SESSION['joueur']);
+$oMaison = $oJoueur->GetObjSaMaison();
 ?>
 
 <div class="main">
 	<h1>Les quêtes</h1>
 	<p>Voici la liste des quêtes qui vous sont proposées. Acceptez une ou plusieurs quêtes et bonne chance!</p>
 	<p><?php echo AfficheIcone('attention');?> Les quêtes ne peuvent s'accomplir que une seule fois. Si vous l'annulée, elle sera perdue.</p>
-	<h2>Quêtes en cours</h2>
-	<?php if(isset($_SESSION['QueteEnCours'])){?>
-		<table class="quetes">
-			<tr>
-			<?php
-			Foreach($_SESSION['QueteEnCours'] as $Quete){
-				echo '<td>'.AfficheAvancementQuete($Quete).'</td>';
-			}
-			?>
-			</tr>
-		</table>
-	<?php }else{?>
-		<p>Vous n'avez aucune Quête en cours.</p>
+	
+	<?php if($oJoueur->GetCoordonnee() != $oMaison->GetCoordonnee()){?>
+		<p class="important"><?php echo AfficheIcone('attention');?>Vous devez vous placer sur votre <img src="./img/carte/<?php echo $oMaison->GetImgName();?>.png" /> pour vous <u>inscrire</u> à une nouvelle quête ou <u>valider</u> une en cours.</p>
 	<?php }?>
-	<h2>Et les quêtes disponibles</h2>
-	<?php 
-	echo SelectQuete($oJoueur);
-	?>
+	
+		<h2>Quêtes en cours</h2>
+		<?php if(isset($_SESSION['QueteEnCours'])){?>
+			<table class="quetes">
+				<tr>
+				<?php
+				Foreach($_SESSION['QueteEnCours'] as $Quete){
+					//echo '<td>'.AfficheAvancementQuete($Quete, $oJoueur).'</td>';
+					echo '<td>'.$Quete->AfficheDescriptif($oJoueur, $oMaison, true, $oJoueur->GetCoordonnee() == $oMaison->GetCoordonnee()).'</td>';
+				}
+				?>
+				</tr>
+			</table>
+		<?php }else{?>
+			<p>Vous n'avez aucune Quête en cours.</p>
+		<?php }?>
+		<h2>Et les quêtes disponibles</h2>
+		<?php
+		echo SelectQuete($oJoueur, $oJoueur->GetCoordonnee() == $oMaison->GetCoordonnee());
+		?>
+	
 </div>
 
 <?php 
+$objManager->UpdateBatiment($oMaison);
 $objManager->update($oJoueur);
-unset($oJoueur);
+unset($oJoueur, $oMaison);
 ?>
