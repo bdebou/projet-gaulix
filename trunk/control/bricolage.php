47bdf9221d125f3fa72664bb4617fe34d259ac8b
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-11-09 16:54:22 +0000

    Remplacement de la variable $_GET par $_POST

--- trunk/control/bricolage.php
+++ trunk/control/bricolage.php
@@ -7,25 +7,25 @@ $oJoueur = $objManager->GetPersoLogin($_SESSION['joueur']);
 
 $ChkDebugVar = false;
 
-if($chkDebug AND $ChkDebugVar){	echo '$_SESSION[\'main\'][\'bricolage\']<br />';print_r($_SESSION['main']['bricolage']);echo '<br />';}
-if($chkDebug AND $ChkDebugVar){	echo '$_GET<br />';print_r($_GET);echo '<br />';}
-if($chkDebug AND $ChkDebugVar){	echo '$_POST<br />';print_r($_POST);echo '<br />';}
+if($chkDebug AND $ChkDebugVar){	var_dump($_SESSION['Bricolage']);}
+if($chkDebug AND $ChkDebugVar){	var_dump($_GET);}
+if($chkDebug AND $ChkDebugVar){	var_dump($_POST);}
 if($chkDebug AND $ChkDebugVar){	echo '<hr />';}
 
 $chkErr = true;
 $CheckRetour = false;
 
-if(isset($_GET['action'])){
-	$RetourOnglet = $_SESSION['main']['bricolage'][$_GET['id']]['type'];
-	$strAnchor = $_SESSION['main']['bricolage'][$_GET['id']]['code'];
-	switch($_GET['action']){
-		case 'fabriquer':				ActionFabriquer($chkErr, $_GET['id'], $oJoueur, $objManager); break;
+if(isset($_POST['action'])){
+	$RetourOnglet = $_SESSION['Bricolage'][$_POST['id']]['type'];
+	$strAnchor = $_SESSION['Bricolage'][$_POST['id']]['code'];
+	switch($_POST['action']){
+		case 'Fabriquer':				ActionFabriquer($chkErr, $_POST['id'], $oJoueur, $objManager); break;
 	}
-	unset($_GET['action']);
+	unset($_POST['action']);
 	$CheckRetour = true;
 }
 
-if($chkDebug AND $ChkDebugVar){print_r($_SESSION['main']['bricolage']);}
+if($chkDebug AND ($ChkDebugVar OR !$chkErr)){var_dump($_SESSION['Bricolage']);}
 
 $objManager->update($oJoueur);
 unset($oJoueur);
