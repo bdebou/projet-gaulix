459a3ee5972c3ec3ab49350703b0f93cee78bfa7
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2012-11-20 12:03:16 +0000

    Extraction de la pub et incorporation dans une iframe pour permettre d'utilise l'attribut target="_blank"

--- view/main.php
+++ view/main.php
@@ -31,15 +31,7 @@ $oJoueur = $objManager->GetPersoLogin($_SESSION['joueur']);
 		</table>
 	</div>
 	<div class="pub">
-		<script type="text/javascript"><!--
-			google_ad_client = "ca-pub-2161674761092050";
-			/* Pub Fight 2 */
-			google_ad_slot = "3236602268";
-			google_ad_width = 300;
-			google_ad_height = 250;
-			//-->
-		</script>
-		<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+		<iframe class="pub" src="./pub.html"></iframe>
 	</div>
 	<div class="actions"><?php AfficheActions($oJoueur);?></div>
 	<div class="history">
