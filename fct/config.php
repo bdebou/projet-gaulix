01b9c56e8adbb0ca084914b524697de1eee8304a
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2013-01-04 12:23:48 +0000

    remplacement des appel de constantes personnage::TYPE_PERFECT_* par objArmement::TYPE_*

--- fct/config.php
+++ fct/config.php
@@ -21,9 +21,9 @@ $temp_combat		= 3600 * 1;				// Temp entre chaque combat
 $chkDebug			= false;
 
 //les couleurs
-$arCouleurs			= array(personnage::TYPE_PERFECT_ATTAQUE	=> '#FF0000',		// Red 
-							personnage::TYPE_PERFECT_DEFENSE	=> '#32CD32',		// LimeGreen
-							personnage::TYPE_PERFECT_DISTANCE	=> '#c0c0c0',
+$arCouleurs			= array(objArmement::TYPE_ATTAQUE			=> '#FF0000',		// Red 
+							objArmement::TYPE_DEFENSE			=> '#32CD32',		// LimeGreen
+							objArmement::TYPE_DISTANCE			=> '#c0c0c0',
 							maison::TYPE_RES_EAU_POTABLE		=> '#92ccff',		// bleu clair
 							maison::TYPE_RES_NOURRITURE			=> '#808000',		// Olive
 							personnage::TYPE_VIE				=> '#FFA07A',		// LightSalmon
