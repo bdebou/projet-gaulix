01816cf49bfdd9a273e18ecea805a970954d244c
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2013-02-14 13:55:22 +0000

    - Utilisation de la constante objRessource:: TYPE_RESSOURCE - Modification du texte

--- trunk/view/regles/inventaire.php
+++ trunk/view/regles/inventaire.php
@@ -1,4 +1,4 @@
 <h1>Inventaire</h1>
-	<p>Liste toutes les choses que vous avez trouvés au cours de vos voyages avec les équipements aussi (voir Equipements).</p>
+	<p>Voici la liste des objets de base que vous pourrez peut=être trouvé lors de vos déplacemenst. Tous ces objets serviront à en fabriquer d'autre dont ceux que vous pouvez retrouver dans les <a href="#" onclick="javascript:change_onglet('equipement');">équipements</a>.</p>
 <h2 style="clear:both;">Les ressources</h2>
-	<?php echo ReglesAfficheTableauEquipements('ressource');?>
+	<?php echo ReglesAfficheTableauEquipements(objRessource::TYPE_RESSOURCE);?>
