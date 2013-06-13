a956feb65994bb848d3fb4d805355a26b52a8b59
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2013-06-13 10:05:53 +0000

    Extracttion de la variable $arColor de config.php pour l'isoler dans arColor.config.php

---
+++ fct/arColor.config.php
@@ -0,0 +1,14 @@
+<?php
+Global $arCouleurs;
+
+//les couleurs
+$arCouleurs	= array(objArmement::TYPE_ATTAQUE			=> '#FF0000',		// Red
+					objArmement::TYPE_DEFENSE			=> '#32CD32',		// LimeGreen
+					objArmement::TYPE_DISTANCE			=> '#c0c0c0',
+					maison::TYPE_RES_EAU_POTABLE		=> '#92ccff',		// bleu clair
+					maison::TYPE_RES_NOURRITURE			=> '#808000',		// Olive
+					personnage::TYPE_VIE				=> '#FFA07A',		// LightSalmon
+					personnage::TYPE_EXPERIENCE			=> '#6495ED',		// CornflowerBlue
+					personnage::TYPE_RES_MONNAIE		=> '#FFD700');		// Gold
+
+?>
