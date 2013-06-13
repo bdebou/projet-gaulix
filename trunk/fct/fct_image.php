6b947e240fdf803a25a430d4a6823bb66277968f
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2013-06-13 10:13:06 +0000

    Utilisation de variable de constante de class pour plus de précision sur la sélection d'un type de ressource

--- trunk/fct/fct_image.php
+++ trunk/fct/fct_image.php
@@ -1,6 +1,13 @@
 <?php
 header ("Content-type: image/png");
 
+function chargerClasse($classname){
+	require $classname.'.class.php';
+}
+spl_autoload_register('chargerClasse');
+
+
+
 /*
 Erreur01:
 	Il n'y a aucun GET value
@@ -11,16 +18,6 @@ Erreur03:
 */
 
 
-$arCouleurs			= array('Attaque'		=> 'FF0000',					// Red 
-							'Defense'		=> '32CD32',					// LimeGreen
-							'Nourriture'	=> '808000',					// Olive
-							'Pierre'		=> '708090',					// SlateGray
-							'Bois'			=> '8B4513',					// SaddleBrown
-							'Hydromel'		=> 'F0E68C',					// Khaki
-							'Vie'			=> 'FFA07A',					// LightSalmon
-							'Experience'	=> '6495ED',					// CornflowerBlue
-							'Or'			=> 'FFD700');					// Gold
-						
 if(!isset($_GET['type'])){ImageErreur('Erreur01');}
 else{
 	switch($_GET['type']){
@@ -29,27 +26,25 @@ else{
 			else{ImageErreur('Erreur03');}
 			break;
 		case 'etatcarte':
-			if(isset($_GET['max']) AND isset($_GET['value'])){ImageStatus($_GET['max'], $_GET['value'], 102, 12);}
+			if(isset($_GET['max']) AND isset($_GET['value'])){ImageStatus($_GET['max'], $_GET['value']);}
 			else{ImageErreur('Erreur03');}
 			break;
-		case 'vie':
-		case 'experience':
+		case personnage::TYPE_VIE:
+		case personnage::TYPE_EXPERIENCE:
 			if(isset($_GET['max']) AND isset($_GET['value']) AND isset($_GET['type'])){
-				$tailleX = 20;
-				$tailleY = 120;
-				if(isset($_GET['taille'])){
+			if(isset($_GET['taille'])){
 					$arTemp = explode('x', $_GET['taille']);
-					$tailleX = $arTemp[0];
-					$tailleY = $arTemp[1];
+					ImageProgression($_GET['type'], $_GET['max'], $_GET['value'], $arTemp[0], $arTemp[1]);
+				}else{
+					ImageProgression($_GET['type'], $_GET['max'], $_GET['value']);
 				}
-				ImageProgression($_GET['type'], $_GET['max'], $_GET['value'], $tailleX, $tailleY);
 			}else{
 				ImageErreur('Erreur03');
 			}
 			break;
 		case 'VieCarte':
 			if(isset($_GET['max']) AND isset($_GET['value'])){
-				ImageProgression('vie', $_GET['max'], $_GET['value'], 70, 14);
+				ImageProgression(personnage::TYPE_VIE, $_GET['max'], $_GET['value'], 70, 14);
 			}else{
 				ImageErreur('Erreur03');
 			}
@@ -62,8 +57,10 @@ else{
 //
 //---------------------------------------------------	FUNCTIONS	-----------------------------
 //
-function ImageProgression($type, $max, $value, $SizeX, $SizeY){
-	global $arCouleurs;
+function ImageProgression($type, $max, $value, $SizeX = 102, $SizeY = 12){
+	//global $arCouleurs;
+	require_once 'arColor.config.php';
+	
 	$image		= imagecreate($SizeX, $SizeY);
 	
 	$icone		= imagecreatefrompng("../img/icones/ic_".strtolower($type).".png");
@@ -77,7 +74,9 @@ function ImageProgression($type, $max, $value, $SizeX, $SizeY){
 	imagecolortransparent($icone_mini, $blanc);
 	
 	//création de la couleur
-	$arExplodeCouleur = str_split($arCouleurs[ucfirst(strtolower($type))], 2);
+	//$arExplodeCouleur = str_split($arCouleurs[ucfirst(strtolower($type))], 2);
+	$arExplodeCouleur = str_split(substr($arCouleurs[$type], 1), 2);
+	
 	foreach($arExplodeCouleur as $col){$colorRVB[] = base_convert($col, 16, 10);}
 	
 	$couleur	= imagecolorallocate($image, $colorRVB['0'], $colorRVB['1'], $colorRVB['2']);
@@ -98,7 +97,7 @@ function ImageProgression($type, $max, $value, $SizeX, $SizeY){
 	imagestring($image, ($SizeY >= 20?4:1), ($LargeurMini + ($SizeY >= 20?14:7)), intval((ImagesY($image) - ($SizeY >= 20?16:8)) / 2), $value.'/'.$max, $noir);
 	return imagepng($image);
 }
-function ImageStatus($max, $value, $SizeX, $SizeY){
+function ImageStatus($max, $value, $SizeX=102, $SizeY=22){
 	$image = imagecreate($SizeX, $SizeY);
 
 	//fond
