dfa0be7d752026e700e075edfbc9dd9986c56b86
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2013-07-04 13:32:16 +0000

    Premier jet pour la gestion des quetes monstres et ennemis

---
+++ trunk/fct/objMonstre.class.php
@@ -0,0 +1,39 @@
+<?php
+class objMonstre extends objMain{
+
+	private $Attaque;
+	private $Defense;
+	private $Distance;
+	
+	public function __construct(array $data, $nb){
+		date_default_timezone_set('Europe/Brussels');
+		
+		parent::Hydrate($data, $nb);
+		
+		foreach($data as $key=>$value)
+		{
+			switch($key)
+			{
+				case 'objet_attaque':		$this->Attaque			= (is_null($value)?0:intval($value));		break;
+				case 'objet_defense':		$this->Defense			= (is_null($value)?0:intval($value));		break;
+				case 'objet_distance':		$this->Distance			= (is_null($value)?0:intval($value));		break;
+			}
+		}
+	}
+	
+	//Les Affichages
+	//==============
+	
+	//Les GETS
+	//========
+	public function GetAttaque(){
+		return $this->Attaque;
+	}
+	public function GetDefense(){
+		return $this->Defense;
+	}
+	public function GetDistance(){
+		return $this->Distance;
+	}
+
+}
--- trunk/fct/qteCombat.class.php
+++ trunk/fct/qteCombat.class.php
@@ -1,10 +1,7 @@
 <?php
 class qteCombat extends quete{
 	
-	Private $Force;
-	Private $Defense;
-	Private $Vie;
-	
+		
 	Const COLOR					= '#ff6464';
 	
 	Const TYPE_QUETE_MONSTRE	= 'Monstre';
@@ -25,109 +22,100 @@ class qteCombat extends quete{
 		
 		foreach ($Quete as $key => $value){
 			switch ($key){
-				case 'quete_vie':			$this->Vie				= intval($value);								break;
+				case 'quete_vie':			self::SetVie(intval($value));		break;
 			}
 		}
 		
 		foreach ($InfoQuete as $key => $value){
 			switch ($key){
-				case 'quete_attaque':		$this->Force			= (is_null($value)?NULL:intval($value));		break;
-				case 'quete_defense':		$this->Defense			= (is_null($value)?NULL:intval($value));		break;
 			}
 		}
 	}
 	public function ActionSurQuete(personnage &$joueur){
 		return NULL;
 	}
-	
-	//On attaque un Monstre de quete
-	/* private function QueteMonstre(personnage &$joueur){
-		$ForceMonstre = $this->Force + ($joueur->GetNiveau() * 3);
-		$arAttaque = $joueur->GetAttPerso();
-		$arDefense = $joueur->GetDefPerso();
-		$txt = '<p>Vous avez attaqué "'.$this->Nom.'".';
-		if((($arAttaque['0'] + $arAttaque['1']) * 1.15) >= $ForceMonstre){
-			//on frappe le monstre car plus fort
-			$ViePerdue = intval(($arAttaque['0'] + $arAttaque['1']) * 1.15);
-			$this->Vie -= $ViePerdue;
-			$txt .= " Il a perdu $ViePerdue pts ".AfficheIcone('vie');
-		}else{$txt .= " Il a perdu aucun pts ".AfficheIcone('vie');
-		}
-		if(($arDefense['0'] + $arDefense['1']) < $ForceMonstre){
-			//On Perd quand meme un peu des pts de vie car le monstre est fort
-			$ViePerdueJoueur = $ForceMonstre - ($arDefense['0'] + $arDefense['1']);
-			$joueur->PerdreVie($ViePerdueJoueur,'quete');
-			$txt .= " mais vous, vous avez perdu $ViePerdueJoueur pts ".AfficheIcone('vie');
-		}
-		if($this->Vie <= 0){
-			$this->FinishQuete();
-			$txt .= " et vous l'avez tué. Bravo!!!";
-		}else{
-			$this->Position = $this->MonstreFuit($joueur);
-			$txt .= " et il s'est enfui.";
-		}
-		return $txt.'</p>';
-	} */
-	
-	/* Private function QueteRomains(personnage &$Joueur){
-		$ForceRomain = $this->Force + ($Joueur->GetNiveau() * 3);
-	
-		$arAttaque = $Joueur->GetAttPerso();
-		$arDefense = $Joueur->GetDefPerso();
-	
-		$txt = '<p>Vous avez attaqué "'.$this->Nom.'".';
-	
-		if((($arAttaque['0'] + $arAttaque['1']) * 1.15) >= $ForceRomain){
-			//on frappe les romains car plus fort
-			$ViePerdue = intval(($arAttaque['0'] + $arAttaque['1']) * 1.15);
-			$this->Vie -= $ViePerdue;
-			$txt .= " Ils ont perdu $ViePerdue pts ".AfficheIcone('vie');
-		}else{$txt .= " Ils ont perdu aucun pts ".AfficheIcone('vie');
-		}
-	
-		if(($arDefense['0'] + $arDefense['1']) < $ForceRomain){
-			//On Perd quand meme un peu des pts de vie car les romains sont forts
-			$ViePerdueJoueur = $ForceRomain - ($arDefense['0'] + $arDefense['1']);
-			$Joueur->PerdreVie($ViePerdueJoueur,'quete');
-			$txt .= " mais vous, vous avez perdu $ViePerdueJoueur pts ".AfficheIcone('vie');
-		}
-	
-		if($this->Vie <= 0){
-			$this->FinishQuete();
-			$txt .= " et vous les avez tués. Bravo!!!";
-			$Joueur->UpdateScores(1, 0);
-		}else{
-			$this->DateCombat = strtotime('now');
-			$txt .= ".";
-		}
-	
-		return $txt.'</p>';
-	} */
-	
-	//on update la quete romain en la déplacant
-	/* private function UpdateQueteRomains(){
-		$nbDep = intval((strtotime('now') - $this->date_start) / 3600);
-	
-		if($nbDep > 0){
+	public function ActionSurQueteCombat(personnage &$joueur){
+		if(	$this->CheckQueteMonstre()
+			OR $this->CheckQueteEnnemi())
+		{
+			//On crée l'objet Monstre ou Ennemi
+			$oEnnemi = $this->FoundEnnemi();
+			
+			If(!is_null($oEnnemi))
+			{
+				$arAttaque = $joueur->GetAttPerso();
+				$arDefense = $joueur->GetDefPerso();
 				
-			if($nbDep > self::MAX_DEPLACEMENT){
-				$nbDep = self::MAX_DEPLACEMENT;
+				$txt = '<p>Vous avez attaqué "'.$oEnnemi->GetNom().'".';
+				
+				if((($arAttaque[0] + $arAttaque[1]) * 1.15) >= $oEnnemi->GetAttaque()){
+					//on frappe le monstre car plus fort
+					$ViePerdue = intval(($arAttaque[0] + $arAttaque[1]) * 1.15) - $oEnnemi->GetDefense();
+					
+					self::SetVie($this->GetVie() - $ViePerdue);
+					
+					$txt .= " Il a perdu $ViePerdue pts ".AfficheIcone(personnage::TYPE_VIE);
+					
+				}else{
+					$txt .= " Il a perdu aucun pts ".AfficheIcone(personnage::TYPE_VIE);
+				}
+				
+				if(($arDefense[0] + $arDefense[1]) < $oEnnemi->GetAttaque()){
+					//On Perd quand meme un peu des pts de vie car le monstre est fort
+					$ViePerdueJoueur = $oEnnemi->GetAttaque() - ($arDefense[0] + $arDefense[1]);
+					
+					$joueur->PerdreVie($ViePerdueJoueur, quete::TYPE_QUETE);
+					
+					$txt .= " mais vous, vous avez perdu $ViePerdueJoueur pts ".AfficheIcone(personnage::TYPE_VIE);
+				}
+				if($this->GetVie() <= 0)
+				{
+					$this->RecupereGains($joueur);
+					$this->FinishQuete();
+					$txt .= " et vous l'avez tué. Bravo!!!";
+				}else{
+					$this->SetPosition($this->MonstreFuit($joueur));
+					$txt .= " et il s'est enfui.";
+				}
+				return $txt.'</p>';
 			}
-				
-			for($i = 0; $i <= $nbDep; $i++){
-				$this->QueteSeDeplaceUneCase();
-			}
-				
-			$this->date_start = strtotime('now');
 		}
-	} */
+		
+		return NULL;
+	}
 	
-	/* private function MonstreFuit(personnage &$joueur){
+	private function FoundEnnemi(){
+		if(	!is_null($this->GetCout()))
+		{
+			foreach($this->GetCout() as $Cout)
+			{
+				$arCout = explode('=', $Cout);
+				
+				if(	$arCout[0] === self::TYPE_QUETE_ENNEMI
+					OR $arCout[0] === self::TYPE_QUETE_MONSTRE)
+				{
+					$oEnnemi = FoundObjet($arCout[1]);
+						
+					if(!is_null($oEnnemi))
+					{
+						return $oEnnemi;
+					}
+				}
+			}
+		}
+		return NULL;
+	}
+	/**
+	 * Retourne une coordonnée libre
+	 * @param personnage $joueur
+	 * @return array(0=>Carte, 1=>X, 2=>Y) 
+	 */
+	private function MonstreFuit(personnage &$joueur){
 		$carte = null;
-		if($joueur->GetNiveau() <= 3){
+		if(!$this->GetCheckCartes()){
 			if(!is_null($joueur->GetMaisonInstalle())){
-				$arcarte = $joueur->GetMaisonInstalle();
-				$carte = $arcarte['0'];
+				$arcarte = explode(',', $joueur->GetMaisonInstalle());
+				$carte = $arcarte[0];
 			}else{
 				$carte = $joueur->GetCarte();
 			}
@@ -135,7 +123,7 @@ class qteCombat extends quete{
 		$free = FreeCaseCarte($carte);
 	
 		return explode(',', $free[array_rand($free)]);
-	} */
+	}
 	
 	//Les Affichages
 	//==============
@@ -146,63 +134,100 @@ class qteCombat extends quete{
 					<div class="fiche_quete">
 						<table class="fiche_quete">
 							<tr style="background:'.$this::COLOR.';">
-								<th>'.$this->Nom.'</th>
+								<th>'.$this->GetNom().'</th>
 							</tr>';
 		if(!$Avancement)
 		{
 			//Si pas encore acceptée, on affiche son cout
 			$txt .= '
 							<tr><th>Coût</th></tr>
-							<tr><td>'.AfficheListePrix($this->Cout, $oJoueur, $oMaison).'</td></tr>';
+							<tr><td>'.AfficheListePrix($this->GetCout(), $oJoueur, $oMaison).'</td></tr>';
 		}else{
 			$txt .= '
-							<tr><td><img alt="Barre de Vie" src="./fct/fct_image.php?type=vie&amp;value=' . $this->GetVie() . '&amp;max=' . $this->GetVieMax() . '" /></td></tr>';
+							<tr><td><img alt="Barre de Vie" src="./fct/fct_image.php?type=vie&amp;value=' . $this->GetVie() . '&amp;max=' . $this->GetVieMax() . '&amp;taille=180x25" /></td></tr>';
 		}
 	
 		//on ajout des infos générale sur la fiche
 		$txt .= '
 							<tr><td class="description">'.$this->GetDescription().'</td></tr>
 							<tr><th>Gains</th></tr>
-							<tr><td>'.AfficheListePrix($this->Gain).'</td></tr>
-							<tr>
-								<td>';
+							<tr><td>'.AfficheListePrix($this->GetGain()).'</td></tr>';
 	
-		if(!$Avancement)
+		if(	!$Avancement
+			AND !is_null($oMaison)
+			AND $oJoueur->GetCoordonnee() == $oMaison->GetCoordonnee())
 		{
 			//on ajoute le boutton ACCEPTER
-			$txt .= '<button type="button"
+			$txt .= '<tr>
+						<td>
+							<button type="button"
 								onclick="window.location=\'index.php?page=quete&amp;action=inscription&amp;num_quete='.$this->GetIDTypeQuete().'\'"' 
-			.((count($_SESSION['QueteEnCours']) < quete::NB_QUETE_MAX AND CheckCout($this->Cout, $oJoueur, $oMaison))?
-			NULL
-			:'disabled=disabled ')
-			.'class="quete" >Accepter</button>';
-		}else{
-			//On ajoute le boutton ANNULER
-			$txt .= '<button type="button"
-								onclick="window.location=\'index.php?page=quete&amp;action=annule&amp;num_quete='.$this->GetIDTypeQuete().'\'" 
-								class="quete" >Annuler</button>';
+								.((count($_SESSION['QueteEnCours']) < quete::NB_QUETE_MAX AND CheckCout($this->GetCout(), $oJoueur, $oMaison))?
+									NULL
+									:'disabled=disabled ')
+								.'class="quete" >S\'inscrire</button>
+						</td>
+					</tr>';
 		}
 	
 		//On ferme la tableau de la ficher quete.
-		$txt .= '
-								</td>
-							</tr>
-						</table>
-					</div>';;
+		$txt .= '		</table>
+					</div>';
 	
 		return $txt;
 	}
-	
+
+	//Les Checks
+	//==========
+	public function CheckQueteMonstre(){
+		if(	!is_null($this->GetCout()))
+		{
+			foreach($this->GetCout() as $Cout)
+			{
+				$arCout = explode('=', $Cout);
+				if($arCout[0] === self::TYPE_QUETE_MONSTRE)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public function CheckQueteEnnemi(){
+		if(	!is_null($this->GetCout()))
+		{
+			foreach($this->GetCout() as $Cout)
+			{
+				$arCout = explode('=', $Cout);
+				if($arCout[0] === self::TYPE_QUETE_ENNEMI)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+		
 	//Les GETS
 	//========
-	public function GetForce(){
-		return $this->Force;
+	public function GetTypeQuete(){
+		if($this->CheckQueteMonstre())
+		{
+			return self::TYPE_QUETE_MONSTRE;
+		}
+		if($this->CheckQueteEnnemi()){
+			return self::TYPE_QUETE_ENNEMI;
+		}
+		return NULL;
 	}
-	public function GetDefense(){
-		return $this->Defense;
-	}
-	public function GetVie(){
-		return $this->Vie;
+	public function GetNomEnnemi(){
+		$oEnnemi = $this->FoundEnnemi();
+		
+		if(!is_null($oEnnemi))
+		{
+			return $oEnnemi->GetNom();
+		}
+		return NULL;
 	}
 }
 ?>
