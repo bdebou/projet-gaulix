436c67fe09d7882991f6cfd33b060358ded9beff
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2014-03-03 14:39:03 +0000

    Ajout du Model/Objet.php pour la gestion des Objets de la DB

--- app/Model/Objet.php
+++ app/Model/Objet.php
@@ -12,9 +12,26 @@
  */
 class Objet extends AppModel{
     
-    public $belongsTo = array('Civilisation');
+    public $belongsTo = array('Civilisation', 'Objtype', 'Objcategory');
     
     public $hasAndBelongsToMany = array('User');
+    
+    public function beforeFind($query){
+        if(isset($query['fields'])){
+            if(!in_array('id', $query['fields']))
+                    $query['fields'][]='id';
+        }
+        return $query;
+    }
+    
+    public function afterFind($results, $primary = false){
+        foreach($results as $k=>$v){
+            if (isset($v['Objet']['id']))
+                $results[$k]['Objet']['link'] = 'objets/'.str_pad($v['Objet']['id'], 5, '0', STR_PAD_LEFT) . '.png';
+        }
+        return $results;
+    }
+    
 }
 
 ?>
