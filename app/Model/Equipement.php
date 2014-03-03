e530f7257dd30256c30f1de8568082cc7c6ce318
bruno.deboubers@gmail.com <bruno.deboubers@gmail.com@9d687742-287b-b972-2aa9-47762cf58f21>
2014-03-03 14:57:54 +0000

    - Ajout du Model Equipement pour gérer la table "equipements" - Ajout du Model Objcategory pour gérer la table "objcategories" - Ajout du Model Objtype pour gérer la table "objtypes"

---
+++ app/Model/Equipement.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of equipement
+ *
+ * @author bdebou
+ */
+class equipement extends AppModel{
+    //put your code here
+}
+
+?>
---
+++ app/Model/Objtype.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Objtype
+ *
+ * @author bdebou
+ */
+class Objtype extends AppModel{
+    
+    public $hasMany = array('Objet');
+    
+    
+}
+
+?>
